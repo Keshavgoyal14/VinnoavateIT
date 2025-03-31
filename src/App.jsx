3f08a4cbf3a8bf9aed1dab46d0f46bd17be282a6
Keshav Goyal <161034870+Keshavgoyal14@users.noreply.github.com>
2025-03-31 23:19:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -108,7 +108,7 @@ const App = () => {
           </SwiperSlide>
           <SwiperSlide>
           <div className="bg-gray-800 p-5 md:h-[480px] rounded-lg shadow-lg flex flex-col md:flex-row  md:items-center justify-space space-x-20 ">
-          <div className='w-[50%] items-center justify-center '> <img src="src\assets\BunkBuddies.png" alt="BunkBuddies" 
+          <div className='w-[50%] items-center justify-center '> <img src="https://www.bing.com/images/blob?bcid=S4eiq5.ESUcIs1YGMTPvQTKDEB.j.....z8" alt="BunkBuddies" 
           className='w-[250px] h-[300px] md:w-[400px] md:h-[400px] mb-10 md:mb-0 rounded-lg m-3 ml-40 shadow-md transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-105'  />
           </div> 
           <div className='w-full mb-10 md:mb-0 md:w-[50%] flex flex-col mr-20 items-center text-center' ><h2 className="text-4xl text-purple-300 font-bold text-center mb-20 transition ease-in-out delay-150 duration-300 hover:translate-y-1 hover:scale-105">BunkBuddies</h2>
